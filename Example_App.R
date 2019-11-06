c1974834edd9d3a2a722477ad1ca62aa5447846e
Amy Pitts <amy.pitts1@marist.edu>
2019-11-06 09:37:16 -0500

    adding quick changes

--- Example_App.R
+++ Example_App.R
@@ -12,23 +12,28 @@ ui <- fluidPage(
     # Sidebar panel for inputs ----
     sidebarPanel(
     
+      #label for the drop down menu
       h6("Choose a Region to focus on"), 
-      # Input: Drop down menu for plot ----
+      
+      # Input: Drop down menu for plot ---- selecting region 
       selectInput(inputId = "region1", label = strong("Region"),
                   choices = c("Northeast" = "Northeast",
                               "South" = "South",
                               "North Central" = "North Central",
                               "West" = "West"),
-                  selected = "Northeast"
-      ),
-      h6("Choose a type of data to be displayed on the y-axis"), 
-      selectInput(inputId = "info", label = strong("Type of Data"),
+                  selected = "Northeast"),
+      
+      #label for the drop down menu
+      h6("Choose a type of data to be displayed on the y-axis"),
+      
+      # Input: Drop down menu for plot ---- selecting type of data
+      radioButtons(inputId = "info", label = strong("Type of Data"),
                   choices = c("Income" = 5,
                               "Life.Exp" = 7,
                               "Murder" = 8,
                               "Area" = 11),
-                              selected = "Income"
-      )),
+                              selected = 5)
+    ),
     
     # Main panel for displaying outputs ----
     mainPanel(
@@ -52,11 +57,11 @@ server <- function(input, output) {
   # 2. Its output type is a plot
   output$distPlot <- renderPlot({
     
-    state.df <- data.frame(state.abb,state.region,state.division,state.x77)
+    state.df <- data.frame(state.abb,state.region,state.division,state.x77) #creating the database
 
-    subset <- state.df[state.region == input$region1,]
+    subset <- state.df[state.region == input$region1,] #this is how we are subsectioning the data by region
     
-    info_st <- input$info
+    info_st <- input$info #this is how we are getting the info from the type of data dropdown 
     
     plot( droplevels(subset$state.abb), subset[,as.numeric(info_st)], 
           xlab="State",
